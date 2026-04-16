78f67a5a6523f838dfbd85acf999f18dbcd03206
Piyush Verma <piyushvermaghga@gmail.com>
2026-04-16 21:30:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import './App.css'
 
 const App = () => {
   return (
